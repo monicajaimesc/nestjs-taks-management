4137af7824b407edda4f38d8a750b5f7e0fb7723
monicajaimesc <monikmaja@gmail.com>
2020-03-25 13:29:34 -0500

    new task created

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './task.model';
 
@@ -13,6 +13,29 @@ export class TasksController {
     getAllTasks(): Task[] {
     // call the service and get the array of tasks
       return this.taskService.getAllTasks();
+    }
+
+    /*
+    first option: retrieve the entire request body
+    @Post()
+    createTask(@Body() body) {
+      console.log('body', body);
+    }
+    */
+
+    /* specifically extract specific body parameters, each decoration before the 
+    parameter we can specify exactly the key in the request body that you want to
+    extract
+    */
+    @Post()
+    createTask(
+      @Body('title') title: string,
+      @Body('description') description: string,
+    ): Task {
+      // get the new desk and return it back to the client
+      return this.taskService.createTask(title, description);
 
     }
+
+
 }
